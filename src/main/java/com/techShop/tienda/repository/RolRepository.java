2a9dc843a530606a5395817c6d1d6f1983de2bb9
Brandon Solis <160693594+brandonso80035@users.noreply.github.com>
2026-04-05 15:33:45 -0600

    Semana 11-12

---
+++ src/main/java/com/techShop/tienda/repository/RolRepository.java
@@ -0,0 +1,13 @@
+package com.techShop.tienda.repository;
+
+
+
+import com.techShop.tienda.domain.Rol;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+
+    public Optional<Rol> findByRol(String rol);
+
+}
